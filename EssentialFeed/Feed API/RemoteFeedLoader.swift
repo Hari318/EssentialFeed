38c5851bd871f1913ea67e742f472075a5705b0b
Hari <harids318@gmail.com>
2023-05-28 12:37:14 +0530

    Moved Remote feed loader into production and updated access specifiers

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,25 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Hari on 28/05/23.
+//
+
+import Foundation
+
+public class RemoteFeedLoader{
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(url: URL, client: HTTPClient){
+        self.url = url
+        self.client = client
+    }
+    public func load(){
+        client.get(from: url)
+    }
+}
+
+public protocol HTTPClient{
+    func get(from url: URL)
+}
