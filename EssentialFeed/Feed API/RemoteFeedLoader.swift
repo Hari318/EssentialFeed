98453de21c2d269998598ad5c2bc869c2d997ab4
Hari <harids318@gmail.com>
2023-06-25 12:01:04 +0530

    Add access specifier for FeedItemMapper

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,35 +7,38 @@
 
 import Foundation
 
-public final class RemoteFeedLoader{
+public final class RemoteFeedLoader {
     private let client: HTTPClient
     private let url: URL
     
-    public enum Error: Swift.Error{
+    public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-    public enum Result: Equatable{
+    public enum Result: Equatable {
         case success([FeedItem])
         case failure(Error)
     }
-    public init(url: URL, client: HTTPClient){
+    public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
-    public func load(completion: @escaping (Result)-> Void){
+    public func load(completion: @escaping (Result)-> Void) {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                do {
-                    let root = try FeedItemsMapper.map(data: data, response: response)
-                    completion(.success(root))
-                } catch {
-                    completion(.failure(.invalidData))
-                }
+                completion(self.map(data, from: response))
             case .failure:
                 completion(.failure(.connectivity))
             }
+        }
+    }
+    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let root = try FeedItemsMapper.map(data: data, response: response)
+            return .success(root)
+        } catch {
+            return .failure(.invalidData)
         }
     }
 }
