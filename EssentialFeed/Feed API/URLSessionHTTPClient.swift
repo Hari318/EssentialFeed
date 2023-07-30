79f4e6c8de222fdbdfbeb3edebc197be0a4a2bcc
Hari <harids318@gmail.com>
2023-07-30 20:50:04 +0530

    Added another way for using URLSession

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -29,3 +29,20 @@ public class URLSessionHTTPClient: HTTPClient {
         }.resume()
     }
 }
+
+//extension URLSession: HTTPClient {
+//    private struct UnExpectedValuesRepresentation: Error {}
+//
+//    public func get(from url: URL, completion: @escaping (HTTPClientResult)-> Void) {
+//        dataTask(with: url) { data, response, error in
+//            if let error = error {
+//                completion(.failure(error))
+//            } else if let data = data, let response = response as? HTTPURLResponse{
+//                completion(.success(data, response))
+//            }
+//            else {
+//                completion(.failure(UnExpectedValuesRepresentation()))
+//            }
+//        }.resume()
+//    }
+//}
