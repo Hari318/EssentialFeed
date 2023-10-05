15d733d88ef67fd15878a0ed97d88ad31810eada
Hari <harids318@gmail.com>
2023-10-05 12:18:00 +0530

    Validate cache command does not delete less than seven days old cache

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-import EssentialFeed
+@testable import EssentialFeed
 
 class ValidateFeedCacheUseCaseTests: XCTestCase {
     
@@ -35,6 +35,19 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_validateCache_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate } )
+        
+        sut.validateCache()
+        
+        store.completeRetrieval(with: feed.local, timeeStamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     //    MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
@@ -45,5 +58,28 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
     }
     private func anyError() -> NSError {
         return NSError(domain: "any error", code: 0)
+    }
+    private func anyURL() -> URL{
+        return URL(string: "https://one.com")!
+    }
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(),
+                        description: "description",
+                        location: "location",
+                        url: anyURL())
+    }
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueImage(), uniqueImage()]
+        let localItems = models.map { return LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (models, localItems)
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
