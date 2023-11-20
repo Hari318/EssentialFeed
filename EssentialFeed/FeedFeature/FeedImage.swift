f64c31731e3a4e2ca7ba85e0f9bcf12ba741b8cb
Hari <harids318@gmail.com>
2023-11-20 14:25:28 +0530

    Render loaded feed on successful load completion

--- EssentialFeed/FeedFeature/FeedImage.swift
+++ EssentialFeed/FeedFeature/FeedImage.swift
@@ -8,8 +8,15 @@
 import Foundation
 
 public struct FeedImage: Equatable{
-    let id: UUID
-    let description: String?
-    let location: String?
-    let url: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+    
+    public init(id: UUID, description: String?, location: String?, url: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.url = url
+    }
 }
