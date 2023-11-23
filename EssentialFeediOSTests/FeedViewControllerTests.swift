3fbb4cf506ec0276479dad9b9699138d8f2560b7
Hari <harids318@gmail.com>
2023-11-23 16:05:47 +0530

    Extract dependency creation and composition logic from `FeedViewController` into a new `FeedUIComposer`

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -271,7 +271,7 @@ final class FeedViewControllerTests: XCTestCase {
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
-        let sut = FeedViewController(feedLoader: loader, imageLoader: loader)
+        let sut = FeedUIComposer.feedComposedWith(feedLoader: loader, imageLoader: loader)
         
         trackMemoryLeaks(instance: sut, file: file, line: line)
         trackMemoryLeaks(instance: loader, file: file, line: line)
