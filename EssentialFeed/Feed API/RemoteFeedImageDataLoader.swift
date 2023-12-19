b145e0276567d3d092a55180c80e3e7dd3707c5f
Hari <harids318@gmail.com>
2023-12-19 19:17:54 +0530

    `LocalFeedImageDataLoader.loadImageData` does not deliver result after cancelling task

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         
         var wrapped: HTTPClientTask?
         
-        init(completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
             self.completion = completion
         }
         
@@ -43,16 +43,30 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     }
     
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPTaskWrapper(completion: completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            task.complete(with: result
-                .mapError { _ in Error.connectivity }
-                .flatMap { (data, response) in
-                    let isValidResponse = response.isOK && !data.isEmpty
-                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
-                })
-        }
-        return task
+        
+        let task = HTTPTaskWrapper(completion)
+                task.wrapped = client.get(from: url) { [weak self] result in
+                    guard self != nil else { return }
+
+                    task.complete(with: result
+                        .mapError { _ in Error.connectivity }
+                        .flatMap { data, response in
+                            let isValidResponse = response.isOK && !data.isEmpty
+                            return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                        })
+                }
+                return task
+        
+//        let task = HTTPTaskWrapper(completion: completion)
+//        task.wrapped = client.get(from: url) { [weak self] result in
+//            guard self != nil else { return }
+//            task.complete(with: result
+//                .mapError { _ in Error.connectivity }
+//                .flatMap { (data, response) in
+//                    let isValidResponse = response.isOK && !data.isEmpty
+//                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+//                })
+//        }
+//        return task
     }
 }
