8625b6e789951361c12ed2e77f0867ba9efa5bb2
Hari <harids318@gmail.com>
2023-12-19 20:14:37 +0530

    Separate retrieval from insertion operations and types to clarify intent

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -45,28 +45,16 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         
         let task = HTTPTaskWrapper(completion)
-                task.wrapped = client.get(from: url) { [weak self] result in
-                    guard self != nil else { return }
-
-                    task.complete(with: result
-                        .mapError { _ in Error.connectivity }
-                        .flatMap { data, response in
-                            let isValidResponse = response.isOK && !data.isEmpty
-                            return isValidResponse ? .success(data) : .failure(Error.invalidData)
-                        })
-                }
-                return task
-        
-//        let task = HTTPTaskWrapper(completion: completion)
-//        task.wrapped = client.get(from: url) { [weak self] result in
-//            guard self != nil else { return }
-//            task.complete(with: result
-//                .mapError { _ in Error.connectivity }
-//                .flatMap { (data, response) in
-//                    let isValidResponse = response.isOK && !data.isEmpty
-//                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
-//                })
-//        }
-//        return task
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { data, response in
+                    let isValidResponse = response.isOK && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
+        }
+        return task
     }
 }
