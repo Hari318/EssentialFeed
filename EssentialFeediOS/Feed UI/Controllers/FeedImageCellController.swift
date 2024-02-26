c9a970396b36a8717f8a81bb175c07313d19f5c0
Hari <harids318@gmail.com>
2024-02-26 22:14:10 +0530

    Replace custom CellController protocol with a composition of the UITableViewDataSource/Delegate/Prefetching, so we can decouple other modules from the Shared UI module

--- EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -13,7 +13,7 @@ public protocol FeedImageCellControllerDelegate {
     func didCancelImageRequest()
 }
 
-public final class FeedImageCellController: CellController, ResourceView, ResourceLoadingView, ResourceErrorView {
+public final class FeedImageCellController: NSObject {
     public typealias ResourceViewModel = UIImage
     private let viewModel: FeedImageViewModel
     private let delegate: FeedImageCellControllerDelegate
@@ -24,22 +24,14 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
         self.delegate = delegate
     }
     
-    public func view(in tableView: UITableView) -> UITableViewCell {
-        cell = tableView.dequeueReusableCell()
-        cell?.locationContainer.isHidden = !viewModel.hasLocation
-        cell?.locationLabel.text = viewModel.location
-        cell?.descriptionLabel.text = viewModel.description
-        cell?.onRetry = delegate.didRequestImage
-        delegate.didRequestImage()
-        return cell!
-    }
-    
-//    public func display(_ viewModel: FeedImageViewModel<UIImage>) {
-//        cell?.onReuse = { [weak self] in
-//            self?.releaseCellForReuse()
-//        }
-//    }
-    
+    //    public func display(_ viewModel: FeedImageViewModel<UIImage>) {
+    //        cell?.onReuse = { [weak self] in
+    //            self?.releaseCellForReuse()
+    //        }
+    //    }
+}
+
+extension FeedImageCellController: ResourceView, ResourceLoadingView, ResourceErrorView {
     public func display(_ viewModel: UIImage) {
         cell?.feedImageView.setImageAnimated(viewModel)
     }
@@ -52,10 +44,6 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
         cell?.feedImageRetryButton.isHidden = viewModel.message == nil
     }
     
-    public func preload() {
-        delegate.didRequestImage()
-    }
-    
     public func cancelLoad() {
         releaseCellForReuse()
         delegate.didCancelImageRequest()
@@ -64,4 +52,33 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
     private func releaseCellForReuse() {
         cell = nil
     }
+}
+
+extension FeedImageCellController: CellController {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell = tableView.dequeueReusableCell()
+        cell?.locationContainer.isHidden = !viewModel.hasLocation
+        cell?.locationLabel.text = viewModel.location
+        cell?.descriptionLabel.text = viewModel.description
+        cell?.onRetry = delegate.didRequestImage
+        delegate.didRequestImage()
+        return cell!
+    }
+    
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        delegate.didRequestImage()
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cancelLoad()
+    }
+    
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        cancelLoad()
+    }
+    
 }
--- EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -8,14 +8,19 @@
 import UIKit
 import EssentialFeed
 
-public class ImageCommentCellController: CellController {
+public class ImageCommentCellController: NSObject, CellController {
+    
     private let viewModel: ImageCommentViewModel
     
     public init (viewModel: ImageCommentViewModel) {
         self.viewModel = viewModel
     }
     
-    public func view(in tableView: UITableView) -> UITableViewCell {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: ImageCommentsCell = tableView.dequeueReusableCell()
         
         cell.messageLabel.text = viewModel.message
@@ -24,4 +29,7 @@ public class ImageCommentCellController: CellController {
         
         return cell
     }
+    
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) { }
+    
 }
